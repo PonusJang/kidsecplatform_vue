13ea37417491acf02b8b6d05df17537ebc879304
K <ponusjang@gmail.com>
2020-11-19 22:49:49 +0800

    2020/11/19 K

--- src/views/osint/index.vue
+++ src/views/osint/index.vue
@@ -18,22 +18,64 @@
 
       <el-tabs tab-position="left" style="height: 90%;">
         <!-- general -->
-        <el-tab-pane label="基本信息" v-model="generalData">
+        <el-tab-pane v-model="generalData" label="基本信息">
           <div v-if="generalData.type == 'IPv4'">
-            经度:{{generalData.longitude}}
-            纬度:{{generalData.latitude}}
-            whois:{{generalData.whois}}
-            国家:{{generalData.country_name}}
-            地区：{{generalData.city}},{{generalData.region}}
-            国旗地址:https://otx.alienvault.com{{generalData.flag_url}}
-            ASN:{{generalData.asn}}
-            信誉值:{{generalData.reputation}}
-            <div v-if="reputation" v-model="reputationData"> {{reputationData}}</div>
+            经度:{{ generalData.longitude }}
+            纬度:{{ generalData.latitude }}
+            whois:{{ generalData.whois }}
+            国家:{{ generalData.country_name }}
+            地区：{{ generalData.city }},{{ generalData.region }}
+            国旗地址:https://otx.alienvault.com{{ generalData.flag_url }}
+            ASN:{{ generalData.asn }}
+            信誉值:{{ generalData.reputation }}
+            <div v-if="reputation" v-model="reputationData"> {{ reputationData }}</div>
           </div>
 
           <div v-if="generalData.type == 'domain'">
-            Alexa:{{generalData.alexa}}
-            whois:{{generalData.whois}}
+            Alexa:{{ generalData.alexa }}
+            whois:{{ generalData.whois }}
+          </div>
+
+          <div v-if="whois">
+            <el-table
+              v-el-table-infinite-scroll="load"
+              height="500px"
+              :data="whoisData.list"
+              element-loading-text="Loading"
+              border
+              fit
+              highlight-current-row
+            >
+              <el-table-column align="center" label="ID" width="70">
+                <template slot-scope="scope">
+                  {{ scope.$index }}
+                </template>
+              </el-table-column>
+              <el-table-column label="RECODE" align="name">
+                <template slot-scope="scope">
+                  <span class="link-type">{{ scope.row.name }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="VALUE" align="name">
+                <template slot-scope="scope">
+                  <span class="link-type">{{ scope.row.value }}</span>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
+
+          <div v-if="generalData.type == 'CVE'">
+
+            description:{{ generalData.description }}
+            mitre_url:{{ generalData.mitre_url }}
+            nvd_url:{{ generalData.nvd_url }}
+            products:{{ generalData.products }}
+            cwe:{{ generalData.cwe }}
+            cvss:{{ generalData.cvss }}
+            cvssv2:{{ generalData.cvssv2 }}
+            cvssv3:{{ generalData.cvssv3 }}
+            date_created:{{ generalData.date_created }}
+            date_modified:{{ generalData.date_modified }}
           </div>
         </el-tab-pane>
         <!-- general -->
@@ -42,6 +84,8 @@
         <el-tab-pane v-if="pulse_info" label="事件情报">
           <div>
             <el-table
+              v-el-table-infinite-scroll="load"
+              height="550px"
               :data="pulse_info_data.list"
               element-loading-text="Loading"
               border
@@ -65,7 +109,7 @@
               </el-table-column>
               <el-table-column label="时间" align="created">
                 <template slot-scope="scope">
-                  <span class="link-type">{{ scope.row.created | parseTime('{y}-{m}-{d} {h}:{i}')  }}</span>
+                  <span class="link-type">{{ scope.row.created }}</span>
                 </template>
               </el-table-column>
               <el-table-column label="操作" align="center" width="250" class-name="small-padding fixed-width">
@@ -88,6 +132,7 @@
         <el-tab-pane v-if="passive_dns" label="反向解析">
           <el-table
             v-el-table-infinite-scroll="load"
+            height="550px"
             :data="passiveDnsData.list"
             element-loading-text="Loading"
             border
@@ -121,12 +166,12 @@
             </el-table-column>
             <el-table-column label="first" align="created">
               <template slot-scope="scope">
-                <span class="link-type">{{ scope.row.first | parseTime('{y}-{m}-{d} {h}:{i}')  }}</span>
+                <span class="link-type">{{ scope.row.first }}</span>
               </template>
             </el-table-column>
             <el-table-column label="last" align="created">
               <template slot-scope="scope">
-                <span class="link-type">{{ scope.row.last | parseTime('{y}-{m}-{d} {h}:{i}')  }}</span>
+                <span class="link-type">{{ scope.row.last }}</span>
               </template>
             </el-table-column>
           </el-table>
@@ -136,6 +181,8 @@
         <!-- url_list -->
         <el-tab-pane v-if="related_urls" label="相关URL">
           <el-table
+            v-el-table-infinite-scroll="load"
+            height="550px"
             :data="reladtedUrlData.list"
             element-loading-text="Loading"
             border
@@ -169,7 +216,7 @@
             </el-table-column>
             <el-table-column label="时间" align="created">
               <template slot-scope="scope">
-                <span class="link-type">{{ scope.row.date | parseTime('{y}-{m}-{d} {h}:{i}')  }}</span>
+                <span class="link-type">{{ scope.row.date }}</span>
               </template>
             </el-table-column>
           </el-table>
@@ -219,7 +266,7 @@
               </el-table-column>
               <el-table-column label="时间" align="created">
                 <template slot-scope="scope">
-                  <span class="link-type">{{ scope.row.datetime_int | parseTime('{y}-{m}-{d} {h}:{i}')  }}</span>
+                  <span class="link-type">{{ scope.row.datetime_int }}</span>
                 </template>
               </el-table-column>
             </el-table>
@@ -266,138 +313,177 @@
 </template>
 
 <script>
-  import {getIndicators} from '@/api/ioc'
-  import {parseTime} from '@/utils'
-  import waves from '@/directive/waves' // waves directive
-  import elTableInfiniteScroll from 'el-table-infinite-scroll';
+import { getIndicators } from '@/api/ioc'
+import { parseTime } from '@/utils'
+import waves from '@/directive/waves' // waves directive
+import elTableInfiniteScroll from 'el-table-infinite-scroll'
 
-  export default {
-    name: 'OsintIndex',
-    directives: {waves, 'el-table-infinite-scroll': elTableInfiniteScroll},
-    data() {
-      return {
-        show: false,
-        passive_dns: false,
-        pulse_info: false,
-        related_urls: false,
-        http_scans: false,
-        related_files: false,
-        malware: false,
-        reputation: false,
+export default {
+  name: 'OsintIndex',
+  directives: { waves, 'el-table-infinite-scroll': elTableInfiniteScroll },
+  data() {
+    return {
+      show: false,
+      passive_dns: false,
+      pulse_info: false,
+      related_urls: false,
+      http_scans: false,
+      related_files: false,
+      malware: false,
+      reputation: false,
+      whois: false,
 
-        query: '',
-        generalData: undefined,
-        malwareData: {
-          list: undefined,
-          count: undefined
-        },
-        reladtedUrlData: {
-          list: undefined,
-          count: undefined
-        },
-        reputationData: undefined,
-        passiveDnsData: {
-          list: undefined,
-          count: undefined
-        },
-        httpScansData: {
-          list: undefined,
-          count: undefined
-        },
-        nidsListData: undefined,
-        whoisData: undefined,
-        screenshotData: undefined,
-        pulse_info_data: {
-          list: undefined,
-          count: undefined
+      query: '',
+      generalData: undefined,
+      malwareData: {
+        list: undefined,
+        count: undefined
+      },
+      reladtedUrlData: {
+        list: undefined,
+        count: undefined
+      },
+      reputationData: undefined,
+      passiveDnsData: {
+        list: undefined,
+        count: undefined
+      },
+      httpScansData: {
+        list: undefined,
+        count: undefined
+      },
+      nidsListData: undefined,
+      whoisData: {
+        list: undefined,
+        count: undefined
+      },
+      screenshotData: undefined,
+      pulse_info_data: {
+        list: undefined,
+        count: undefined
+      }
+
+    }
+  },
+  methods: {
+    formatJson(filterVal) {
+      return this.list.map(v => filterVal.map(j => {
+        if (j === 'add_time') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
+    },
+    reset() {
+      this.show = false,
+      this.passive_dns = false,
+      this.pulse_info = false,
+      this.related_urls = false,
+      this.http_scans = false,
+      this.related_files = false,
+      this.malware = false,
+      this.reputation = false,
+
+      this.generalData = undefined,
+      this.malwareData.list = undefined,
+
+      this.reladtedUrlData.list = undefined,
+      this.reladtedUrlData.count = undefined
+
+      this.reputationData = undefined,
+      this.passiveDnsData.list = undefined,
+      this.count = undefined,
+
+      this.httpScansData.list = undefined,
+      this.httpScansData.count = undefined,
+
+      this.nidsListData = undefined,
+      this.whoisData.list = undefined,
+      this.whoisData.count = undefined,
+      this.screenshotData = undefined,
+      this.pulse_info_data.list = undefined,
+      this.pulse_info_data.count = undefined
+    },
+    handleQuery() {
+      this.reset()
+      getIndicators(this.query, undefined).then(res => {
+        this.show = true
+        this.generalData = res.data
+
+        if (res.data.pulse_info !== undefined) {
+          if (res.data.pulse_info.count) {
+            console.log(res.data.pulse_info.pulses)
+            this.pulse_info = true
+            this.pulse_info_data.list = res.data.pulse_info.pulses
+            this.pulse_info_data.count = res.data.count
+          }
         }
 
-      }
-    },
-    methods: {
-      formatJson(filterVal) {
-        return this.list.map(v => filterVal.map(j => {
-          if (j === 'add_time') {
-            return parseTime(v[j])
-          } else {
-            return v[j]
-          }
-        }))
-      },
-      handleQuery() {
-        getIndicators(this.query, undefined).then(res => {
-          this.show = true
-          this.generalData = res.data
-
-          if (res.data.pulse_info !== undefined) {
+        if (res.data.sections.indexOf('malware') !== -1) {
+          getIndicators(this.query, 'malware').then(res => {
             if (res.data.count) {
-              this.pulse_info = true
-              this.pulse_info_data.list = res.data.pulse_info.pulses
-              this.pulse_info_data.count = res.data.count
+              this.malware = true
+              this.malwareData.count = res.data.count
+              this.malwareData.list = res.data.data
             }
-          }
-
-          if (res.data.sections.indexOf('malware') !== -1) {
-            getIndicators(this.query, 'malware').then(res => {
-              if (res.data.count) {
-                this.malware = true
-                this.malwareData.count = res.data.count
-                this.malwareData.list = res.data.data
-              }
-            })
-          }
-          if (res.data.sections.indexOf('reputation') !== -1) {
-            getIndicators(this.query, 'reputation').then(res => {
-              this.reputationData = res.data
-            })
-          }
-          if (res.data.sections.indexOf('url_list') !== -1) {
-            getIndicators(this.query, 'url_list').then(res => {
-              if (res.data.actual_size) {
-                this.related_urls = true
-                this.reladtedUrlData.count = res.data.actual_size
-                this.reladtedUrlData.list = res.data.url_list
-              }
-            })
-          }
-          if (res.data.sections.indexOf('passive_dns') !== -1) {
-            getIndicators(this.query, 'passive_dns').then(res => {
-              if (res.data.count) {
-                this.passive_dns = true
-                this.passiveDnsData.count = res.data.count
-                this.passiveDnsData.list = res.data.passive_dns
-              }
-            })
-          }
-          if (res.data.sections.indexOf('http_scans') !== -1) {
-            getIndicators(this.query, 'http_scans').then(res => {
-              this.http_scans = res.data
-              if (res.data.count) {
-                this.http_scans = true
-                this.httpScansData.count = res.data.count
-                this.httpScansData.list = res.data.data
-              }
-            })
-          }
-          if (res.data.sections.indexOf('nids_list') !== -1) {
-            getIndicators(this.query, 'nids_list').then(res => {
-              this.nidsListData = res.data
-            })
-          }
-          if (res.data.sections.indexOf('whois') !== -1) {
-            getIndicators(this.query, 'whois').then(res => {
-              this.whoisData = res.data
-            })
-          }
-          if (res.data.sections.indexOf('screenshot') !== -1) {
-            getIndicators(this.query, 'screenshot').then(res => {
-              this.screenshotData = res.data
-            })
-          }
-        })
-      }
+          })
+        }
+        if (res.data.sections.indexOf('reputation') !== -1) {
+          getIndicators(this.query, 'reputation').then(res => {
+            this.reputationData = res.data
+          })
+        }
+        if (res.data.sections.indexOf('url_list') !== -1) {
+          getIndicators(this.query, 'url_list').then(res => {
+            if (res.data.actual_size) {
+              this.related_urls = true
+              this.reladtedUrlData.count = res.data.actual_size
+              this.reladtedUrlData.list = res.data.url_list
+            }
+          })
+        }
+        if (res.data.sections.indexOf('passive_dns') !== -1) {
+          getIndicators(this.query, 'passive_dns').then(res => {
+            if (res.data.count) {
+              this.passive_dns = true
+              this.passiveDnsData.count = res.data.count
+              this.passiveDnsData.list = res.data.passive_dns
+            }
+          })
+        }
+        if (res.data.sections.indexOf('http_scans') !== -1) {
+          getIndicators(this.query, 'http_scans').then(res => {
+            if (res.data.count) {
+              this.http_scans = true
+              this.httpScansData.count = res.data.count
+              this.httpScansData.list = res.data.data
+            }
+          })
+        }
+        if (res.data.sections.indexOf('nids_list') !== -1) {
+          getIndicators(this.query, 'nids_list').then(res => {
+            this.nidsListData = res.data
+          })
+        }
+        if (res.data.sections.indexOf('whois') !== -1) {
+          getIndicators(this.query, 'whois').then(res => {
+            if (res.data.count) {
+              this.whois = true
+              this.whoisData.list = res.data.data
+              this.whoisData.count = res.data.count
+            }
+          })
+        }
+        if (res.data.sections.indexOf('screenshot') !== -1) {
+          getIndicators(this.query, 'screenshot').then(res => {
+            this.screenshotData = res.data
+          })
+        }
+      })
     }
   }
+}
 </script>
 
 <style scoped>
