2e09c72441f2dba98b86dc263884f111335a39d3
Ponus Cheung <zhangsopor@gmail.com>
2020-11-23 17:50:22 +0800

    20201123 TODO PRIV

---
+++ src/api/menu.js
@@ -0,0 +1,43 @@
+import request from '@/utils/request'
+
+export function getList(page, limit) {
+  return request({
+    url: '/menu/getList',
+    method: 'get',
+    params: { page, limit }
+  })
+}
+
+export function findByID(page, limit, id) {
+  return request({
+    url: '/menu/findByID',
+    method: 'get',
+    params: { page, limit ,id }
+  })
+}
+
+
+export function add(data) {
+  return request({
+    url: '/menu/add',
+    method: 'post',
+   data
+  })
+}
+
+
+export function del(id) {
+  return request({
+    url: '/menu/del',
+    method: 'get',
+    params: { id }
+  })
+}
+
+
+export function getMenu() {
+  return request({
+    url: '/menu/getMenu',
+    method: 'get',
+  })
+}
--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -3,7 +3,7 @@
   <div class="app-container">
     <div class="filter-container">
       <el-input
-        v-model="listQuery.rolename"
+        v-model="listQuery.id"
         placeholder="角色名"
         style="width: 200px;"
         class="filter-item"
@@ -42,27 +42,69 @@
       fit
       highlight-current-row
     >
-      <el-table-column align="center" label="ID" width="95">
+      <!--            <el-table-column align="center" label="ID" width="95">-->
+      <!--              <template slot-scope="scope">-->
+      <!--                {{ scope.$index }}-->
+      <!--              </template>-->
+      <!--            </el-table-column>-->
+      <el-table-column label="菜单ID" align="center">
         <template slot-scope="scope">
-          {{ scope.$index }}
+          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.id }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="角色名" align="center">
+      <el-table-column label="菜单名称" align="center">
         <template slot-scope="scope">
-          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.roleName }}</span>
+          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.name }}</span>
         </template>
       </el-table-column>
 
-      <el-table-column label="描述" align="center">
+      <el-table-column label="父菜单" align="center">
         <template slot-scope="scope">
-          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.description }}</span>
+          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.pid }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="路径" align="center">
+        <template slot-scope="scope">
+          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.path }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="组件" align="center">
+        <template slot-scope="scope">
+          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.component }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="重定向" align="center">
+        <template slot-scope="scope">
+          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.redirect }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="标题" align="center">
+        <template slot-scope="scope">
+          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.meta.title }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="图标" align="center">
+        <template slot-scope="scope">
+          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.meta.icon }}</span>
         </template>
       </el-table-column>
 
-      <el-table-column label="操作" align="center" width="250" class-name="small-padding fixed-width">
+      <el-table-column label="子菜单" align="center">
+        <template slot-scope="scope">
+          <span class="link-type" @click="handleUpdate(row)"> {{ scope.row.children }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="隐藏" align="center">
+        <template slot-scope="scope">
+          <span v-if="scope.row.hidden">是</span>
+          <span v-if="!scope.row.hidden">否</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="" align="center" width="250" class-name="small-padding fixed-width">
         <template slot-scope="{row,$index}">
           <el-button type="primary" size="mini" @click="handleUpdate(row)">
-            编辑权限
+            编辑
           </el-button>
           <el-button size="mini" type="danger" @click="handleDelete(row,$index)">
             删除
@@ -89,22 +131,35 @@
         label-width="70px"
         style="width: 400px; margin-left:50px;"
       >
-        <el-form-item label="角色名" prop="title">
-          <el-input v-model="temp.roleName"/>
+        <el-form-item label="ID" prop="title">
+          <el-input v-model="temp.id"/>
         </el-form-item>
-        <el-form-item label="描述" prop="title">
-          <el-input v-model="temp.description"/>
+        <el-form-item label="名称" prop="title">
+          <el-input v-model="temp.name"/>
         </el-form-item>
-        <el-form-item label="权限" prop="title">
-          <el-tree
-            :data="menu"
-            show-checkbox
-            default-expand-all
-            node-key="id"
-            ref="tree"
-            highlight-current
-            :props="defaultProps">
-          </el-tree>
+        <el-form-item label="父菜单" prop="title">
+          <el-input v-model="temp.pid"/>
+        </el-form-item>
+        <el-form-item label="路径" prop="title">
+          <el-input v-model="temp.path"/>
+        </el-form-item>
+        <el-form-item label="组件" prop="title">
+          <el-input v-model="temp.component"/>
+        </el-form-item>
+        <el-form-item label="重定向" prop="title">
+          <el-input v-model="temp.redirect"/>
+        </el-form-item>
+        <el-form-item label="标题" prop="title">
+          <el-input v-model="temp.meta.title"/>
+        </el-form-item>
+        <el-form-item label="图标" prop="title">
+          <el-input v-model="temp.meta.icon"/>
+        </el-form-item>
+        <el-form-item label="子菜单" prop="title">
+          <el-input v-model="temp.children"/>
+        </el-form-item>
+        <el-form-item label="隐藏" prop="title">
+          <el-switch v-model="temp.hidden"></el-switch>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -122,14 +177,12 @@
 
 <script>
   // eslint-disable-next-line no-unused-vars
-  import {getList, update, add, del, findByRole} from '@/api/role'
-  import {getMenu} from '@/api/menu'
-
+  import {getList, add, del,findByID } from '@/api/menu'
   import {parseTime} from '@/utils'
   import waves from '@/directive/waves' // waves directive
   import Pagination from '@/components/Pagination/index' // secondary package based on el-pagination
   export default {
-    name: 'SystemList',
+    name: 'menuList',
     components: {Pagination},
     directives: {waves},
     filters: {
@@ -137,11 +190,6 @@
     },
     data() {
       return {
-        defaultProps: {
-          children: 'children',
-          label: 'name'
-        },
-        menu: undefined,
         total: 0,
         listQuery: {
           page: 1,
@@ -159,14 +207,20 @@
         },
         dialogPvVisible: false,
         rules: {
-          roleName: [{required: true, message: '角色名 is required', trigger: 'blur'}]
+          id: [{required: true, message: 'ID is required', trigger: 'blur'}]
         },
         downloadLoading: false,
         temp: {
           id: undefined,
-          roleName: '',
-          description: '',
-          privilege: []
+          name:'',
+          path:'#',
+          component:'#',
+          redirect:undefined,
+          pid:undefined,
+          meta:{
+            title:'',
+            icon:''
+          }
         }
       }
     },
@@ -175,13 +229,9 @@
     },
     methods: {
       getList() {
-        getMenu().then(res => {
-          console.log(res.data)
-          this.menu = res.data
-        })
         this.listLoading = false
-        if (this.listQuery.roleName !== undefined && this.listQuery.roleName !== '') {
-          findByRole(this.listQuery.page, this.listQuery.limit, this.listQuery.roleName).then(response => {
+        if (this.listQuery.id !== undefined && this.listQuery.id !== '') {
+          findByID(this.listQuery.page, this.listQuery.limit, this.listQuery.id).then(response => {
             this.total = response.data.count
             this.list = response.data.docs
             setTimeout(() => {
@@ -201,7 +251,7 @@
       resetTemp() {
         this.temp = {
           id: undefined,
-          roleName: ''
+          meta:{title: '', icon:''}
         }
       },
       createData() {
@@ -221,7 +271,7 @@
         })
       },
       handleDelete(row, index) {
-        del(row.roleName).then(() => {
+        del(row.id).then(() => {
           this.$notify({
             title: 'Success',
             message: 'Delete Successfully',
@@ -250,11 +300,9 @@
         this.dialogFormVisible = true
         this.$nextTick(() => {
           this.$refs['dataForm'].clearValidate()
-          this.$refs.tree.setCheckedKeys(row.privilege);
         })
       },
       updateData() {
-        this.temp.privilege = this.$refs.tree.getCheckedKeys()
         this.$refs['dataForm'].validate((valid) => {
           if (valid) {
             const tempData = Object.assign({}, this.temp)
@@ -275,13 +323,13 @@
       handleDownload() {
         this.downloadLoading = true
         import('@/vendor/Export2Excel').then(excel => {
-          const tHeader = ['roleName']
-          const filterVal = ['roleName']
+          const tHeader = ['id']
+          const filterVal = ['id']
           const data = this.formatJson(filterVal)
           excel.export_json_to_excel({
             header: tHeader,
             data,
-            filename: '角色列表'
+            filename: '菜单列表'
           })
           this.downloadLoading = false
         })
